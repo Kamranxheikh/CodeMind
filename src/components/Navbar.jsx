39e9796edb8e030f3666ad0229ec5d43874eb3a2
Kamranxheikh <kamranshakh841@gmail.in>
2024-09-23 13:31:02 +0500

    fix:Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Menu, X } from 'lucide-react'
 import { navItems } from "../constants"
 import { useState } from 'react'
+import logo from "../assets/logo.png"
 
 
 const Navbar = () => {
@@ -17,7 +18,9 @@ const Navbar = () => {
               <div className='container px-4 mx-auto relative text-sm'>
                 <div className='flex  justify-between items-center'>
                     <div className="flex item-center  flex-shrink-0">
-                    <img className='h-10 w-10 mr-2 ' src="/public/asset/logo.png" alt="" />
+                    <img className='h-10 w-10 mr-2 ' src={logo} alt="logo" /> 
+                    
+                        
                     <span className='text-xl tracking-tight mt-2'>CodeMinds</span>
                     </div>
                     <ul className='hidden lg:flex ml-14 space-x-12 '>
@@ -69,3 +72,28 @@ const Navbar = () => {
 }
 
 export default Navbar
+// const MyComponent = (parameters) => {
+//     // You can have state or other logic here using hooks
+
+//     return (
+//         <>
+//             <nav className=" sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+//                 <div className='container px-4 mx-auto relative text-sm'>
+//                     <div className='flex  justify-between items-center'>
+//                         <div className="flex item-center flex-shrink-0">
+//                             <img className='h-10 w-10 mr-2' src="/public/asset/logo.png" alt="logo" />
+//                             <span className='text-xl tracking-tight mt-2'>CodeMinds</span>
+//                         </div>
+//                         <ul className='hidden lg:flex ml-14 space-x-12'>
+//                             {navItems.map((item, index) => (
+//                                 <li key={index} className=''>
+//                                     <a href={item.href}>{item.label}</a>
+//                                 </li>
+//                             ))}
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </nav>
+//         </>
+//     );
+// };
